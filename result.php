00a9c42fee122807ac093a45f1d1a7fd9d33a960
Tiago Murakami <trmurakami@gmail.com>
2016-06-16 12:49:14 -0300

    Ordem na data

--- result.php
+++ result.php
@@ -200,7 +200,7 @@ $total = $cursor["hits"]["total"];
                             gerar_faceta($query_aggregate,$escaped_url,colab_instituicao_trab,30,"Colaboração - Instituição");
                             gerar_faceta($query_aggregate,$escaped_url,colab_instituicao_corrigido,30,"Colaboração - Instituição - Corrigido");
                             gerar_faceta($query_aggregate,$escaped_url,colab_instituicao_naocorrigido,30,"Colaboração - Instituição - Não corrigido");
-                            gerar_faceta($query_aggregate,$escaped_url,dataregistro,30,"Data de registro e alterações");
+                            gerar_faceta($query_aggregate,$escaped_url,dataregistro,30,"Data de registro e alterações","desc");
                         ?>
                             
                             
