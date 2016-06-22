837130229dde311dffbfca976860fd1580619c7d
Tiago Murakami <trmurakami@gmail.com>
2016-06-22 14:17:26 -0300

    Melhoria no comparador SCOPUS

--- inc/functions.php
+++ inc/functions.php
@@ -515,12 +515,12 @@ function compararRegistrosScopus ($query_type,$query_year,$query_title,$query_au
         $row[]= $results["_score"];
         $row[]= $results["_id"];
         $result_row = implode("\\t", $row);
-        $result_row = preg_replace( "/\r|\n|'/", "", $result_row );
+        $result_row = preg_replace( "/\r|\n|\'/", "", $result_row );
         return $result_row;
         }
     } else {
             $row = ''.$query_year.'\\t'.$query_type.'\\t'.$query_title.'\\t'.$query_DOI.'\\t'.$query_authors.'\\tNão encontrado\\tNão encontrado\\tNão encontrado\\tNão encontrado\\tNão encontrado\\tNão encontrado\\tNão encontrado';
-            $row = preg_replace( "/\r|\n/", "", $row );        
+            $result_row = preg_replace( "/\r|\n|\'/", "", $result_row );       
             return $result_row;
     }
 }
