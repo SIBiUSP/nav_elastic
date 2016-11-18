778e66b3416efdde970c705dffc840c8632b3e56
Tiago Murakami <trmurakami@gmail.com>
2016-11-18 13:21:02 -0200

    Inicio da ferramenta de tradução

---
+++ Locale/pt_BR/LC_MESSAGES/pt_BR.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'domain' => NULL,
+  'plural-forms' => 'nplurals=2; plural=(n != 1);',
+  'messages' => 
+  array (
+    '' => 
+    array (
+      '' => 
+      array (
+        0 => 'Project-Id-Version: BDPI
+Report-Msgid-Bugs-To: tiago.murakami@st.sibi.usp.br
+Last-Translator: Tiago Murakami <tiago.murakami@dt.sibi.usp.br>
+Language-Team: SIBiUSP
+MIME-Version: 1.0
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: 8bit
+POT-Creation-Date: 2016-11-18T13:17:22-02:00
+PO-Revision-Date: 2016-11-18
+Language: pt_BR
+Plural-Forms: nplurals=2; plural=(n != 1);
+',
+      ),
+      'Todas as bases' => 
+      array (
+        0 => 'Todas as bases',
+      ),
+      'Buscar' => 
+      array (
+        0 => 'Buscar',
+      ),
+    ),
+  ),
+);
--- index.php
+++ index.php
@@ -34,6 +34,8 @@
         <?php include_once("inc/analyticstracking.php") ?>
         <?php include('inc/navbar.php'); ?>
         
+        
+        
         <div class="uk-grid uk-margin-large-bottom" data-uk-grid-margin>
             <div class="uk-width-medium-1-1">
                 <div class="uk-vertical-align uk-text-center uk-responsive-width" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 350px;">
@@ -45,7 +47,7 @@
                                 <legend>Pesquisa</legend>
                                 <input type="text" placeholder="Pesquise por termo ou autor" class="uk-form-width-large" name="search[]" data-validation="required">                                        
                                 <select name="search[]">
-                                    <option disabled selected value>Todas as bases</option>
+                                    <option disabled selected value><?php echo $t->gettext('Todas as bases'); ?></option>
                                     <option value="base.keyword:&quot;Produção científica&quot;">Produção científica</option>
                                     <option value="base.keyword:&quot;Teses e dissertações&quot;">Teses e dissertações</option>
                                 </select>
@@ -54,7 +56,7 @@
                                 <input type="hidden" name="fields[]" value="authorUSP">
                                 <input type="hidden" name="fields[]" value="subject">
                                 <input type="hidden" name="fields[]" value="resumo">
-                                <button class="uk-button-primary">Buscar</button><br/>                                    
+                                <button class="uk-button-primary"><?php echo $t->gettext('Buscar'); ?></button><br/>                                    
                             </fieldset>
                         </form>
                     </div>
