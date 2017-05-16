dd969c787dfec7431dbbf3079340b04181d4d122
Tiago Murakami <trmurakami@gmail.com>
2017-05-16 14:26:50 -0300

    Página Sobre

--- sobre.php
+++ sobre.php
@@ -14,10 +14,11 @@
 
         <?php include('inc/navbar.php'); ?>
 <div class="uk-container uk-margin-large-top">
-    <h1>Em breve</h1>
-        
-        
-        
+    <h1>Sobre</h1>
+        <p>Esta é uma interface desenvolvida especialmente para consulta aos registros das partituras cadastradas no Banco de dados Bibliográficos da USP  Dédalus.  As informações e formas de busca foram adaptadas para melhor atender às necessidades de quem procura esse tipo de  documento  musical, sejam pesquisadores ou intérpretes.</p>
+        <p>O maior acervo de partituras cadastrado no Dédalus  pertence  à Biblioteca da Escola de Comunicações e Artes, que forma e organiza documentos musicais desde a década de 1970. Os registros do Dédalus  vieram da base de dados Acorde, desenvolvida pela Biblioteca da ECA  e que tem suas origens num pioneiro catálogo automatizado criado em 1978. A coleção é composta por obras de música erudita de compositores brasileiros e estrangeiros de várias épocas e estilos, partituras de música folclórica e popular, obras didáticas para ensino de música. Os destaques são as obras de música sacra colonial, originárias das cidades mineiras de Ayuruoca, Brasópolis e Campanha e as partituras manuscritas de importantes compositores brasileiros como Henrique Oswald, Fructuoso Vianna, Fúrio Franceschini, Gilberto Mendes, e Osvaldo Lacerda.</p>
+        <p>O empréstimo é facultado a todos os usuários USP, de acordo com as normas do Empréstimo Unificado.  As obras manuscritas e as partituras raras podem ser consultadas apenas no espaço da Biblioteca da ECA.</p>        
+                       
         <hr class="uk-grid-divider">
             
      
