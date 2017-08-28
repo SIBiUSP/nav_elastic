4a5b33101a29973ab64963c6e8192fc389c84f39
Tiago Murakami <trmurakami@gmail.com>
2017-08-28 12:40:26 -0300

    Jquery local

--- inc/meta-header.php
+++ inc/meta-header.php
@@ -3,10 +3,10 @@
 <link rel="shortcut icon" href="inc/images/faviconUSP.ico" type="image/x-icon">
 
 <!-- Jquery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script> 
+<script src="inc/jquery/jquery.min.js"></script>
+<script src="inc/jquery/jquery-ui.js"></script>
+<link rel="stylesheet" href="inc/jquery/jquery-ui.css">
+<script src="inc/jquery/jquery.form-validator.min.js"></script> 
 
 <!-- Uikit 3 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.18/css/uikit.min.css" />
