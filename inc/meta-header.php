3c4539e4a53f903e3fca743bb98b4ab23efae4a8
Tiago Murakami <trmurakami@gmail.com>
2017-08-28 12:20:49 -0300

    Uikit Local

--- inc/meta-header.php
+++ inc/meta-header.php
@@ -8,10 +8,16 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script> 
 
-<!-- Uikit 3 -->
+<!-- Uikit 3 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.18/css/uikit.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.18/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.18/js/uikit-icons.min.js"></script>
+-->
+
+<!-- Uikit - Local -->
+<link rel="stylesheet" href="inc/uikit/css/uikit.min.css" />
+<script src="inc/uikit/js/uikit.min.js"></script>
+<script src="inc/uikit/js/uikit-icons.min.js"></script>
 
 <!-- USP Custom -->
 <link rel="stylesheet" href="inc/css/style.css">
