f40dd15d64324556ff52d503847c90a4f6fb23fd
Tiago Murakami <trmurakami@bdpife2.sibi.usp.br>
2017-10-03 15:30:14 -0300

    Exclusão de metadados da base

--- delete_autoridades.php
+++ delete_autoridades.php
@@ -1,69 +1,38 @@
 <?php
-
+include('inc/config.php'); 
 include('inc/functions.php');
 
-$query='
-{
-  "fields" : ["_id"],
-  "query": {
-    "filtered": {
-      "query": {
-        "match_all": {}
-      },
-      "filter": {
-        "bool": {
-          "must": [
-                {
-                "term":{
-                    "colab_instituicao_tematres":true
-                }
-            }
-          ],
-          "must_not": [
-             {"term":
-                {"colab_instituicao_corrigido" : ""}
-             }
-          ]
-        }
-      }
-    }
-  },
-  "size":10000
+$query["query"]["query_string"]["query"] = "+_exists_:USP.opencitation";    
+$query['sort'] = [
+    ['datePublished.keyword' => ['order' => 'desc']],
+];      
+
+$params = [];
+$params["index"] = $index;
+$params["type"] = $type;
+$params["size"] = 1000;
+$params["body"] = $query;
+
+$cursor = $client->search($params);
+$total = $cursor["hits"]["total"];
+print_r($total);
+echo '<br/><br/>';
+
+
+foreach ($cursor["hits"]["hits"] as $r) {
+
+  unset($r["_source"]["USP"]["opencitation"]);
+
+  //print_r($r["_source"]);
+
+  $query_update["doc"] = $r["_source"];
+  $query_update["doc_as_upsert"] = true;  
+
+  elasticsearch::elastic_delete($r["_id"],$type); 
+  $resultado = elasticsearch::elastic_update($r["_id"],$type,$query_update);  
+  print_r($resultado);
+  echo '<br/><br/>';
+
 }
-';
-
-$cursor = query_elastic($query);
-
-
-/*
-$query_delete = '
-{
-    "doc" : {
-        "colab_instituicao_tematres" : false
-    }
-}
-';
-
-*/
-
-
-$query_delete = '
-{
-    "doc" : {
-        "colab_instituicao_corrigido" : "",
-        "colab_instituicao_naocorrigido" : "",
-        "colab_instituicao_geocode" : "",
-        "colab_instituicao_tematres" : false
-    }
-}
-';
-
-
-
-foreach ($cursor["hits"]["hits"] as $colab) {
-    update_elastic($colab["_id"],$query_delete);
-}
-
-
 
 ?>
