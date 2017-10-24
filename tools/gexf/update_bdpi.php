19430ffc62094a43f5ace76f6f8dd92224e91bbf
Tiago Murakami <trmurakami@bdpife2.sibi.usp.br>
2017-10-24 13:48:28 -0200

    Correção

--- tools/gexf/update_bdpi.php
+++ tools/gexf/update_bdpi.php
@@ -1,7 +1,7 @@
 <?php
 include ('../../inc/config.php');
 echo $_GET["search"];
-$get_gexf = file_get_contents(''.$url_base.'/gexf.php?'.$_SERVER["QUERY_STRING"].'');
+$get_gexf = file_get_contents(''.$url_base.'/tools/gexf.php?'.$_SERVER["QUERY_STRING"].'');
 $sha1 = sha1($_SERVER["QUERY_STRING"]);
 file_put_contents('./data/bdpi-'.$sha1.'.gexf', $get_gexf);
 
