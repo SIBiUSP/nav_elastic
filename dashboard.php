d2c99c14d2c966b55195a9788980787d10314aee
Tiago Murakami <trmurakami@bdpife2.sibi.usp.br>
2018-06-14 05:42:29 -0300

    Path em variavel

--- dashboard.php
+++ dashboard.php
@@ -19,7 +19,7 @@
 		<?php include('inc/navbar.php'); ?>
         <div class="uk-container uk-margin-large-top">
             <h1>Dashboard</h1>           
-            <iframe src="http://<?php echo $kibana; ?>:5601/app/kibana#/dashboard/7893a2a0-1656-11e8-bf7a-b7437c2dde01?embed=true&_g=()" height="8500" width="100%"></iframe>
+            <iframe src="http://<?php echo $kibana; ?>:5601/app/kibana#/dashboard/<?php echo $dashboardHash; ?>?embed=true&_g=()" height="8500" width="100%"></iframe>
         </div>
         <?php include('inc/footer.php'); ?>
         
