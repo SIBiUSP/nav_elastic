bf6cd5bcdf05244059d63a3685036a2853bae853
Tiago <tiago@bdpife2.sibi.usp.br>
2019-04-01 10:43:29 -0300

    Correção no harvester bitstreams

--- admin/harvester_bitstreams.php
+++ admin/harvester_bitstreams.php
@@ -8,7 +8,6 @@ $oaiUrl = $oaiDspace;
 $client_harvester = new \Phpoaipmh\Client(''.$oaiUrl.'');
 $myEndpoint = new \Phpoaipmh\Endpoint($client_harvester);
 
-
 $recs = $myEndpoint->listRecords('xoai', null, null, "col_BDPI_4");
 
 foreach ($recs as $rec) {
@@ -16,25 +15,25 @@ foreach ($recs as $rec) {
     $json = json_encode($rec);
     $array = json_decode($json, true);
     foreach ($array["metadata"]["metadata"]["element"]as $element) {
-      switch ($element["@attributes"]["name"]) {
-        case "usp":
-            $sysno = $element["element"][0]["element"]["field"];
-            $itemID = DSpaceREST::searchItemDSpace($sysno, $DSpaceCookies);
-            $bitstreamsDSpace = DSpaceREST::getBitstreamDSpace($itemID, $DSpaceCookies);
-            $cursor = elasticsearch::elastic_get($sysno, $type, null);
-            if (isset($cursor["_source"]["USP"]["fullTextFiles"])) {
-              if (count($bitstreamsDSpace) != count($cursor["_source"]["USP"]["fullTextFiles"])) {
-                $body["doc"]["USP"]["fullTextFiles"] =  $bitstreamsDSpace;
-                $resultUpdateFilesElastic = elasticsearch::elastic_update($sysno, $type, $body);
-                unset($body);
-              }
-            } else {
-              $body["doc"]["USP"]["fullTextFiles"] =  $bitstreamsDSpace;
-              $resultUpdateFilesElastic = elasticsearch::elastic_update($sysno, $type, $body);
-              unset($body);
-            }
-            break;
-      }
+		switch ($element["@attributes"]["name"]) {
+			case "usp":
+				$sysno = $element["element"][0]["element"]["field"];
+				$itemID = DSpaceREST::searchItemDSpace($sysno, $_SESSION["DSpaceCookies"]);
+				$bitstreamsDSpace = DSpaceREST::getBitstreamDSpace($itemID, $_SESSION["DSpaceCookies"]);
+				$cursor = elasticsearch::elastic_get($sysno, $type, null);
+				if (isset($cursor["_source"]["USP"]["fullTextFiles"])) {
+					if (count($bitstreamsDSpace) != count($cursor["_source"]["USP"]["fullTextFiles"])) {
+						$body["doc"]["USP"]["fullTextFiles"] =  $bitstreamsDSpace;
+						$resultUpdateFilesElastic = elasticsearch::elastic_update($sysno, $type, $body);
+						unset($body);
+					}
+				} else {
+					$body["doc"]["USP"]["fullTextFiles"] =  $bitstreamsDSpace;
+					$resultUpdateFilesElastic = elasticsearch::elastic_update($sysno, $type, $body);
+					unset($body);
+				}
+			break;
+		}
     }
 }
 
