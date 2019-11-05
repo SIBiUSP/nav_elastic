2cceacefe8aee2f3d7f9db3d9d74acb7c217c018
jan-ll <janleduc@usp.br>
2019-11-05 17:12:54 -0300

    xss attack prevention

--- result.php
+++ result.php
@@ -13,11 +13,10 @@
  * @link     https://github.com/SIBiUSP/nav_elastic
  */
 require 'inc/config.php';
-/*
+
 array_walk_recursive($_GET, function (&$item, $key){
     $item = htmlspecialchars($item);
 });
-*/
 
 if (isset($_GET["search"])) {
     foreach ($_GET["search"] as $getSearch) {
