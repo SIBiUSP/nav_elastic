a7faead4f1c6622e5c1a9b1b4d7bdf2ca7cf323b
Alander <alandermm@hotmail.com>
2019-12-05 11:04:26 -0300

    Mapa de calor desativado

--- inc/meta-header.php
+++ inc/meta-header.php
@@ -15,12 +15,3 @@
     <link rel="stylesheet" href="<?php echo $url_base;?>/inc/css/style.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <script type='text/javascript'>
-      window.smartlook||(function(d) {
-        var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-        var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-        c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
-        })(document);
-        smartlook('init', 'acda19d3e9dcbeaaa21f8b5bb14541dd623caaa7');
-    </script>
