1e5ec18b62d494346567a05fe1adfef92274117e
Alander <alandermm@hotmail.com>
2020-01-24 18:20:05 -0300

    Adição dos campos cidade, estado e país. Adição de cabeçalho na mensagem.

--- contact.php
+++ contact.php
@@ -17,10 +17,18 @@
         <?php require 'inc/navbar.php'; ?>
         <div class="uk-container uk-margin-large-top" style="position: relative; padding-bottom: 15em;">
             <?php
-                if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["mensagem"])){
+                if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["mensagem"]) && isset($_POST["cidade"])){
                     $nome = input_sanitize($_POST["nome"]);
                     $email_cliente = input_sanitize($_POST["email"], true);
+                    $cidade = input_sanitize($_POST["cidade"]);
+                    $estado = input_sanitize($_POST["estado"]);
+                    $pais = input_sanitize($_POST["pais"]);
                     $mensagem = input_sanitize($_POST["mensagem"]);
+                    $mensagem_email = "Nome: {$nome}\n";
+                    $mensagem_email .= "E-mail: {$email_cliente}\n";
+                    $mensagem_email .= "Cidade/UF: {$cidade}/{$estado}\n";
+                    $mensagem_email .= "País: {$pais}\n";
+                    $mensagem_email .= "Mensagem:\n{$mensagem}\n";
                     $email_remetente = "Atendimento AGUIA <atendimento@aguia.usp.br>";
                     $email_destinatario = $email_remetente;
                     $assunto = "Contato do site do Repositório - $nome";
@@ -29,7 +37,7 @@
                     $headers .= "From: $email_remetente\n";
                     $headers .= "Return-Path: $email_destinatario\n";
                     $headers .= "Reply-To: $nome <$email_cliente>\n";
-                    if(filter_var($email_cliente, FILTER_VALIDATE_EMAIL) && mail($email_destinatario, $assunto, $mensagem, $headers)){
+                    if(filter_var($email_cliente, FILTER_VALIDATE_EMAIL) && mail($email_destinatario, $assunto, $mensagem_email, $headers)){
                         $notification = $t->gettext('A sua mensagem foi enviada com sucesso!');
                         echo "<div class=\"uk-alert-success\" uk-alert>
                             <a class=\"uk-alert-close\" uk-close></a>
@@ -64,6 +72,21 @@
                                 <label class="uk-form-label">E-mail</label>
                                 <div class="uk-form-controls">
                                     <input type="email" name="email" placeholder="<?php echo $t->gettext('Digite o seu e-mail'); ?>" class="uk-input uk-width-1-1">
+                                </div>
+                            </div>
+
+                            <div class="uk-form-row">
+                                <label class="uk-form-label"><?php echo $t->gettext('Cidade'); ?></label>
+                                <div class="uk-width-1-2@s">
+                                    <input class="uk-input" name="cidade" type="text" placeholder="<?php echo $t->gettext('Digite a sua cidade'); ?>">
+                                </div>
+                                <label class="uk-form-label"><?php echo $t->gettext('Estado'); ?></label>
+                                <div class="uk-width-1-4@s">
+                                    <input class="uk-input" name="estado" type="text" placeholder="<?php echo $t->gettext('Digite o seu estado'); ?>">
+                                </div>
+                                <label class="uk-form-label"><?php echo $t->gettext('País'); ?></label>
+                                <div class="uk-width-1-4@s">
+                                    <input class="uk-input" name="pais" type="text" placeholder="<?php echo $t->gettext('Digite o seu país'); ?>">
                                 </div>
                             </div>
 
@@ -110,7 +133,9 @@
         <hr class="uk-grid-divider">
 
         </div>
-        <?php require 'inc/footer.php'; ?>
+        <div style="position: relative; max-width: initial;">
+            <?php require 'inc/footer.php'; ?>
+        </div>
 
 <?php include('inc/offcanvas.php'); ?>
 
