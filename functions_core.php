42b4c8c386cddecc2b8b552044356bd472b0ca5d
Alander Marques Machado <alander.machado@aguia.usp.br>
2020-03-25 09:44:34 -0300

    Atualizar o elastic

--- functions_core.php
+++ functions_core.php
@@ -801,6 +801,28 @@ class authorities {
 }
 
 /**
+ * ElasticPatch
+ *
+ *@category Class
+ *@package ElasticPatch
+ */
+class ElasticPatch
+{
+    static function syncElastic($sysno)
+    {
+	$url = "http://dspaceapi:8000/item/".$sysno."/";
+	$headers = array('Content-Type: application/json');
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	$response = curl_exec($curl);
+	curl_close($curl);
+    }
+}
+
+/**
  * DSpaceREST
  *
  * @category Class
