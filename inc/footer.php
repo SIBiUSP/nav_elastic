6028ce35f31d7a5262be950c093b64a46e8347b1
Alander Marques Machado <alander.machado@aguia.usp.br>
2022-05-06 18:35:34 -0300

    Remoção tutorial

--- inc/footer.php
+++ inc/footer.php
@@ -19,18 +19,6 @@
 	                    <a class="uk-link-muted" href="<?php echo $url_base; ?>/advanced_search.php"><?php echo $t->gettext('Busca técnica'); ?></a>
 	                </li>
 
-		        	<!--<li>
-	                <?php if (!empty($_SESSION['oauthuserdata'])) : ?>
-	                    <!-- < ?php if (is_staffUser()) : ?> -->
-	                        <a href="<?php echo $url_base; ?>/tutorial_staff/" target="_blank" rel="noopener noreferrer"><?php echo $t->gettext('Tutorial Staff'); ?></a>
-	                    <!-- < ?php else: ?> -->
-	                    <!--    <a href="< php echo $url_base; ?>/tutorial/" target="_blank" rel="noopener noreferrer">< ?php echo $t->gettext('Tutorial'); ?></a> -->
-	                    <!-- < ?php endif; ?>
-	                <?php else: ?>
-	                    <a href="<?php echo $url_base; ?>/tutorial/" target="_blank" rel="noopener noreferrer"><?php echo $t->gettext('Tutorial'); ?></a>
-	                <?php endif; ?>
-	                </li>-->
-
 	                <li>
 	                    <a href="<?php echo $url_base; ?>/contact.php"><?php echo $t->gettext('Contato'); ?></a>
 	                </li>
