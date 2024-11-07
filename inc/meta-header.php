01818a69bb7fdf70f23d4fd55bba3a1adc8638fd
Suporte ABCD <suporte@abcd.usp.br>
2024-11-07 17:57:22 -0300

    Adição do plugin de acessibilidade contratado pela STI/USP

--- inc/meta-header.php
+++ inc/meta-header.php
@@ -1,3 +1,4 @@
+    <?php require_once('functions.php');?>
 
     <link rel="shortcut icon" href="<?=$url_base?>/inc/images/faviconUSP.ico" type="image/x-icon">
 
@@ -16,4 +17,24 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+    
+    <?php if(!is_bdta()): ?>
+        <!-- Perto Digital -->
+        <script>
+            !function
+            e(){"complete"===document.readyState?window.setTimeout((function(){
+            var
+            e,n,t;e="https://cdn.pertoplugin.link/plugin/perto.js.gz",n=5e3,t=
+            "https://pertocdn.pertoplugin.link/plugin/perto.js.gz",new
+            Promise((function(o,i){var
+            c=document.createElement("script");function
+            r(){if(document.head.removeChild(c),t){var
+            e=document.createElement("script");e.src=t,document.head.appendChild(
+            e),e.onload=function(){o(!0)},e.onerror=function(){o(!1)}}else
+            o(!1)}c.src=e,document.head.appendChild(c);var
+            u=setTimeout((function(){r()}),n);c.onload=function(){clearTimeout(u)
+            ,o(!0)},c.onerror=function(){clearTimeout(u),r()}}))}),2e3):
+            window.setTimeout((function(){e()}),1e3)}();
+        </script>
+    <?php endif;?>
